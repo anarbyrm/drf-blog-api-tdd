fc12b18a847989191e77a448db0c7fac887aebdc
Anar Bairam <anar.b@labrin.net>
2022-12-29 20:39:08 +0400

    user model and tests completed

--- project/blog_users/models.py
+++ project/blog_users/models.py
@@ -1,18 +1,25 @@
+from django.db import models
 from django.contrib.auth.models import (
                         AbstractBaseUser,
                         PermissionsMixin,
                         BaseUserManager
                         )
-from django.db import models
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
@@ -20,7 +27,6 @@ class User(PermissionsMixin, AbstractBaseUser):
     email = models.EmailField(max_length=225, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
--- project/blog_users/tests/test_models.py
+++ project/blog_users/tests/test_models.py
@@ -4,7 +4,6 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     """Tests for User models"""
-
     def test_if_user_can_be_created_by_email(self):
         """Tests if user object can be created by email"""
         email = 'test@example.com'
@@ -13,7 +12,15 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-
         self.assertTrue(user.check_password(password))
         self.assertEqual(user.email, email)
-        
+    
+    def test_if_superuser_can_be_created_by_email(self):
+        email = 'superuser@example.com'
+        password = 'test12345'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password= password
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
